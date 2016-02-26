cddf089143c747663509f4b04ee4ebc26c29fbf8
lethosor <lethosor@gmail.com>
2016-02-25 23:08:39 -0500

    A few instruction-minimizing optimizations

--- snake.c
+++ snake.c
@@ -7,12 +7,12 @@
 #define DIMX 96
 #define DIMY 64
 
-#define SNAKE_BUFFER_SIZE 100
-__at 0x9872 int8_t snake_body[BUFFER_SIZE];
+#define SNAKE_BUFFER_SIZE 200
+__at 0x9872 uint8_t snake_body[BUFFER_SIZE];
 uint16_t snake_size;
 uint16_t snake_body_offset; // 0 to (SNAKE_BUFFER_SIZE - 1)
-int8_t snake_head_x;
-int8_t snake_head_y;
+uint8_t snake_head_x;
+uint8_t snake_head_y;
 
 #define MAX_OBJECTS 10
 int8_t objects[MAX_OBJECTS * 2];
@@ -20,8 +20,8 @@ int8_t objects[MAX_OBJECTS * 2];
 enum Direction { up, down, left, right };
 int8_t direction;
 
-#define pixel_on(buf, x, y) buf[y*12 + x/8] |= (1 << (7-x%8))
-#define pixel_off(buf, x, y) buf[y*12 + x/8] &= ~(1 << (7-x%8))
+#define pixel_on(buf, x, y) buf[y*12 + x/8] |= (1 << (7 - (x & 0x07)))
+#define pixel_off(buf, x, y) buf[y*12 + x/8] &= ~(1 << (7 - (x & 0x07)))
 
 void move_snake() {
     int16_t volatile head, tail;
@@ -34,7 +34,7 @@ void move_snake() {
     switch (direction) {
     case up:
         snake_head_y--;
-        if (snake_head_y < 0)
+        if (snake_head_y == 255)
             snake_head_y = DIMY - 1;
         break;
     case down:
@@ -44,7 +44,7 @@ void move_snake() {
         break;
     case left:
         snake_head_x--;
-        if (snake_head_x < 0)
+        if (snake_head_x == 255)
             snake_head_x = DIMX - 1;
         break;
     case right:
@@ -57,14 +57,14 @@ void move_snake() {
     snake_body[head + 1] = snake_head_y;
     snake_body_offset = head;
     pixel_on(plotSScreen, snake_body[head], snake_body[head + 1]);
-    if (snake_body[tail] >= 0)
+    if (snake_body[tail] != 255)
         pixel_off(plotSScreen, snake_body[tail], snake_body[tail + 1]);
     FastCopy();
 }
 
 int main() {
     uint8_t key;
-    memset(snake_body, -1, SNAKE_BUFFER_SIZE);
+    memset(snake_body, 255, SNAKE_BUFFER_SIZE);
     snake_size = 5;
     snake_body_offset = snake_size * 2;
     snake_head_x = DIMX / 2;
